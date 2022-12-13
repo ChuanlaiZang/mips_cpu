4f4ba07c6f13a7149918a8419363cb4f800233c0
ChuanlaiZang <chuanlaizang@gmail.com>
2022-12-13 17:44:20 +0900

    mips_cpu

---
+++ fpga/mips_cpu.ip_user_files/ip/single_port_rom/single_port_rom_stub.vhdl
@@ -0,0 +1,33 @@
+-- Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2018.3 (win64) Build 2405991 Thu Dec  6 23:38:27 MST 2018
+-- Date        : Tue Dec 13 17:11:09 2022
+-- Host        : Byron running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               c:/Users/chuan/Documents/GitHub/mips_cpu/src/ip/single_port_rom/single_port_rom_stub.vhdl
+-- Design      : single_port_rom
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7z020clg400-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity single_port_rom is
+  Port ( 
+    clka : in STD_LOGIC;
+    ena : in STD_LOGIC;
+    addra : in STD_LOGIC_VECTOR ( 9 downto 0 );
+    douta : out STD_LOGIC_VECTOR ( 31 downto 0 )
+  );
+
+end single_port_rom;
+
+architecture stub of single_port_rom is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clka,ena,addra[9:0],douta[31:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "blk_mem_gen_v8_4_2,Vivado 2018.3";
+begin
+end;
